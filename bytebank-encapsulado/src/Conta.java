a7699b49c17c02305f0b6c7ed212fea9ca8f6580
Wagner Casa <pessoalwagner@gmail.com>
2021-06-01 21:21:21 -0300

    criacao  de testesaca negativo e encapsulamento

--- bytebank-encapsulado/src/Conta.java
+++ bytebank-encapsulado/src/Conta.java
@@ -2,12 +2,10 @@
 public class Conta {
 	
 	private double saldo;
-	int agencia;
-	int numero;
-	Cliente titular;
-	
+	private int agencia;
+	private int numero;
+	private Cliente titular;
 
-		
 	
 
 	// METADOS
@@ -35,10 +33,36 @@ public class Conta {
 		
 	}
 	
+	// Encapasulamento
 	
 	public double getSaldo() {
 		return saldo;
 	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public Cliente getTitular() {
+		return titular;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
 
 	
 	
---
+++ bytebank-encapsulado/src/TestaGetESet.java
@@ -0,0 +1,14 @@
+
+public class TestaGetESet {
+	public static void main(String[] args) {
+		
+		Conta conta = new Conta();
+		
+		// AULA 5.5
+		
+		conta.setNumero(1337);
+		System.out.println(conta.getNumero()); 
+		
+	}
+
+}

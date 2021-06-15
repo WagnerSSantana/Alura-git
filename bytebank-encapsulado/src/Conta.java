e5eb984dd6bd945586ecfbc682ced3fb6d29defa
Wagner Silva Santana <wagner.santana@derba.local>
2021-06-15 17:54:45 -0300

    aleração do projeto encapsulado e crianção do herdado

--- bytebank-encapsulado/src/Conta.java
+++ bytebank-encapsulado/src/Conta.java
@@ -1,95 +1,83 @@
-
 public class Conta {
-	
+
 	private double saldo;
 	private int agencia;
 	private int numero;
 	private Cliente titular;
-	private static int total;
-	
-	//Construtor
-	
+	private static int total = 0;
 
+	//CONSTRUTOR
 	public Conta(int agencia, int numero) {
 		Conta.total++;
 		System.out.println("O total de contas é " + Conta.total);
 		this.agencia = agencia;
 		this.numero = numero;
-		System.out.println("estou criando uma conta " + this.numero);
+		this.saldo = 100;
+		System.out.println("Estou criando uma conta " + this.numero);
 	}
 
-	
-	
-	// METADOS
-	public void deposita (double valor) {
-		this.saldo += valor;
+	//METADOS
+	public void deposita(double valor) {
+		this.saldo = this.saldo + valor;
 	}
 
-	public boolean saca (double valor) {
+	public boolean saca(double valor) {
 		if (this.saldo >= valor) {
 			this.saldo -= valor;
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
-	
+
 	public boolean transfere(double valor, Conta destino) {
 		if (this.saldo >= valor) {
 			this.saldo -= valor;
 			destino.deposita(valor);
 			return true;
-		}else {
-			return false;
 		}
-		
+		return false;
 	}
-	
-	// Encapasulamento
-	
+
+	//ENCAPSULAMENTO
 	public double getSaldo() {
-		return saldo;
+		return this.saldo;
+	}
+
+	public int getNumero() {
+		return this.numero;
+	}
+
+	public void setNumero(int numero) {
+		if (numero <= 0) {
+			System.out.println("Nao pode valor menor igual a 0");
+			return;
+		}
+		this.numero = numero;
 	}
 
 	public int getAgencia() {
-		return agencia;
+		return this.agencia;
 	}
 
 	public void setAgencia(int agencia) {
-		if (agencia<=0) {
-			System.out.println("Não pode valor menor igual a 0");
+		if (agencia <= 0) {
+			System.out.println("Nao pode valor menor igual a 0");
 			return;
 		}
 		this.agencia = agencia;
 	}
 
-	public int getNumero() {
-		return numero;
-	}
-
-	public void setNumero(int numero) {
-		if (numero<=0) {
-			System.out.println("Não pode valor menor igual a 0");
-			return;
-			
-		}
-		this.numero = numero;
-	}
-
-	public Cliente getTitular() {
-		return titular;
-	}
-
 	public void setTitular(Cliente titular) {
 		this.titular = titular;
 	}
-	
+
+	public Cliente getTitular() {
+		return this.titular;
+	}
+
 	public static int getTotal() {
 		return Conta.total;
 	}
-	
-
-	
-	
 
 }
